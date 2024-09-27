bdb4bcf1b733b0aefe5ecff0aabfc39bbfb19bab
Magnus Lopes <65486321+magnuslopes23@users.noreply.github.com>
2024-09-27 14:16:47 +0100

    Using Function

--- euler.js
+++ euler.js
@@ -1,10 +1,31 @@
-var sum = 0;
-for(var i = 0; i < 1000;i++ ){
-    if(i %3 ==0 || i % 5 ==0 ){
-        sum += i;
-    }
-}
-console.log(sum);
-alert('Numberrrr is:' + sum);
+// 1. Find the sum of all the multiples of 3 or 5 below 1000.
 
-console.log(sum);
+// var sum = 0;
+// for(var i = 0; i < 1000;i++ ){
+//     if(i %3 ==0 || i % 5 ==0 ){
+//         sum += i;
+//     }
+// }
+// console.log(sum);
+// alert('Numberrrr is:' + sum);
+
+// console.log(sum);
+
+// 2. Create a function to find the sum of all the multiples of a or b below n.
+// (a,b,n) are function parameters
+
+var sum = 0;
+let myFunction = (a , b, n) =>{
+    for(var i = 0; i < n;i++ ){
+            if(i % a ==0 || i % b ==0 ){
+                sum += i;
+            }
+        }
+        console.log(sum);
+}
+
+ alert("By using function "+ myFunction(3, 5, 10));
+
+//  3. Allow the user to enter the parameters via text box(es)
+// and alert them to the output
+
