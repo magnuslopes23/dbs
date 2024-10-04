798b38cdcdd7f832a6af8959fe73cf4cb28a9a42
Magnus Lopes <65486321+magnuslopes23@users.noreply.github.com>
2024-10-04 14:12:35 +0100

    dom changes

---
+++ dom.js
@@ -0,0 +1,8 @@
+function calSum(){
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+
+    var num3 = num1 + num2;
+
+    document.getElementsByClassName("sum").innerHTML = "The Sum is: " + sum;
+}
