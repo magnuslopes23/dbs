1383d7a4686b4d3232f0c34b8be858b04e912dda
Magnus Lopes <65486321+magnuslopes23@users.noreply.github.com>
2024-10-04 14:07:25 +0100

    js changes

--- euler.js
+++ euler.js
@@ -31,11 +31,13 @@ let myFunction = (a , b, n) =>{
 //  3. Allow the user to enter the parameters via text box(es)
 // and alert them to the output
 
-var num1 = parseInt(document.getElementById("num1").value);
-var num2 = parseInt(document.getElementById("num2").value);
-var num3 = parseInt(document.getElementById("num3").value);
+
 
 function doIt(){
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    var num3 = parseInt(document.getElementById("num3").value);
+    
     console.log(myFunction(num1, num2, num3));
     alert("By user input  "+ myFunction(num1, num2, num3));
 }
