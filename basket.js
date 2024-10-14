e66d3ccbfea864f8586af223c6277ad2b9309f6c
Magnus Lopes <65486321+magnuslopes23@users.noreply.github.com>
2024-10-14 11:00:35 +0100

    alert to display on page

--- basket.js
+++ basket.js
@@ -10,13 +10,16 @@ let basketCost = (basket, price) => {
 
 function calculate () {
     let basket = {
-        "banana": parseInt(document.getElementById("product1").value), "bread": parseInt(document.getElementById("product2").value), "milk": parseInt(document.getElementById("product3").value),
+        "banana": parseInt(document.getElementById("product1").value), 
+        "bread": parseInt(document.getElementById("product2").value), 
+        "milk": parseInt(document.getElementById("product3").value),
     }
     
     let price = {
         "banana": 1.5, "bread": 2.99, "milk": 3.50,
     }
     
+    let totalCost = basketCost(basket, price);
+    document.getElementById("basketTotal").innerHTML = `Total Cost of basket is: ${totalCost}`;
     
-    alert(basketCost(basket, price));
 }
