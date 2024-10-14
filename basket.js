37c3949c02a2f0377e08dcab181c0c287c9adaeb
Magnus Lopes <65486321+magnuslopes23@users.noreply.github.com>
2024-10-14 10:41:07 +0100

    dom

--- basket.js
+++ basket.js
@@ -1,10 +1,22 @@
 let basketCost = (basket, price) => {
 
     let total = 0;
-    for(let product in basket){
-        total += basket[product]*price[product];
+    for (let product in basket) {
+        total += basket[product] * price[product];
     }
 
     return total;
 }
-alert(basketCost({ "banana": 2, "bread": 4, "milk": 1 }, { "banana": 1.5, "bread": 2.99, "milk": 3.50, }))
+
+function calTotal () {
+    let basket = {
+        "banana": parseInt(document.getElementById("product1").value), "bread": parseInt(document.getElementById("product2").value), "milk": parseInt(document.getElementById("product3").value),
+    }
+    
+    let price = {
+        "banana": 1.5, "bread": 2.99, "milk": 3.50,
+    }
+    
+    
+    alert(basketCost(basket, price));
+}
